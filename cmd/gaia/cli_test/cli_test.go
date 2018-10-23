d7a2d05ab2ce964a762e6cace6ae5ed27438f76a
Alessio Treglia <alessio@tendermint.com>
2018-10-22 17:37:28 -0700

    Fix integration tests

--- cmd/gaia/cli_test/cli_test.go
+++ cmd/gaia/cli_test/cli_test.go
@@ -487,7 +487,7 @@ func TestGaiaCLISendGenerateSignAndBroadcast(t *testing.T) {
 	// Test sign --print-sigs
 	success, stdout, _ = executeWriteRetStdStreams(t, fmt.Sprintf(
 		"gaiacli tx sign %v --print-sigs %v", flags, unsignedTxFile.Name()))
-	require.True(t, success)
+	require.False(t, success)
 	require.Equal(t, fmt.Sprintf("Signers:\n 0: %v\n\nSignatures:\n", fooAddr.String()), stdout)
 
 	// Test sign
